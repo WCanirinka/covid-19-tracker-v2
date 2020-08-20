5236867c9b1a7e1a9d659d6e6bf89bc10fa668f1
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-08-20 22:26:15 +0200

    Add graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,5 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
+// import classes from '*.module.css';
+import numeral from 'numeral';
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: 'index',
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format('+0,0');
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          format: 'MM/DD/YY',
+          tooltipFormat: 'll',
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format('0a');
+          },
+        },
+      },
+    ],
+  }
+}
+
+const buildChartData = (data, casesType = 'cases') => {
+  const chartData = [];
+  let lastDataPoint;
+
+  for(let date in data.cases) {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint
+      }
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+}
 
 function LineGraph() {
   const [data, setData] = useState({});
@@ -7,20 +70,34 @@ function LineGraph() {
   // https://disease.sh/v3/covid-19/historical/all?lastdays=120
 
   useEffect(() => {
-    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-    .then(response => response.json())
-    .then(data => {
-      
-    })
-  }, [])
+    const fetchData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+      .then(response => response.json())
+      .then(data => {
+        const chartData = buildChartData(data, 'cases');
+        setData(chartData);
+      })
+    }
+    fetchData();
+  }, []);
+
 
   return (
     <div>
       <h3>I am a graph</h3>
-      {/* <Line 
-        data
-        options
-      /> */}
+      {data?.length > 0 && (
+        <Line 
+          data = {{
+            
+            datasets: [{
+              backgroundColor: 'rgba(204, 16, 52, 0.5)',
+              borderColor: '#CC1034',
+              data: data,
+            }],
+          }}
+          options={options}
+        />
+      )}
     </div>
   )
 }
