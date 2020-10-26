d17c2c6baa005d7177383202619a351a79c327f3
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-10-26 15:55:08 +0200

    Revert "Covid19 Tracker"

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
-import './InfoBox.css';
 
-function InfoBox({ title, cases, isRed, active, total, ...props }) {
+function InfoBox({ title, cases, total }) {
     return (
-      <Card onClick={props.onClick} className={`infoBox ${active && 'infoBox--selected'} ${isRed && 'infoBox--red'}`}>
+      <Card>
         <CardContent>
-          <Typography className='infoBox__title' color='textSecondary'>{title}</Typography>
-        <h2 className={`infoBox__cases ${!isRed && 'infoBox__cases--green'}`}>{cases}</h2>
-          <Typography className='infoBox__total' color='textSecondary'>
+          <Typography color='textSecondary'>{title}</Typography>
+          <h2>{cases}</h2>
+          <Typography color='textSecondary'>
             {total} Total
           </Typography>
         </CardContent>
